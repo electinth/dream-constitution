d502205e0ada76cdfe7b1f93ed5699caa8d41114
Th1nkK1D <Th1nkK1D@users.noreply.github.com>
2021-07-27 14:02:56 +0000

    deploy: 45820760c236a99a6036956027d6ff44a0589a85

---
+++ _next/static/vsOy4MVXmq45AJcWG0pQF/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Ftopics\u002F[topicNumber]\u002Fresult","\u002Ftopics\u002F[topicNumber]","\u002Ftopics\u002F[topicNumber]\u002Fquiz"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
