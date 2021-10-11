2fa1d9f84bb8d6a0499a191b08fd1a28a3ebebde
Th1nkK1D <Th1nkK1D@users.noreply.github.com>
2021-10-11 13:50:54 +0000

    deploy: f6d1f1db4174b7c7d633ae891231ff42c1ad9928

---
+++ _next/static/8IPfON1-2sqRBSpkdQ3yE/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Finternal\u002Fgenerate-og\u002F[topicNumber]","\u002Ftopics\u002F[topicNumber]\u002Fquiz","\u002Ftopics\u002F[topicNumber]","\u002Ftopics\u002F[topicNumber]\u002Fresult"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
